b52145eece0e079d352611d52cbbcbcf13c1f6f3
Ivanov1979 <isanhuezaantris@byupathway.edu>
2026-06-06 18:51:45 -0400

    Update database connection to use DB_URL

--- src/models/db.js
+++ src/models/db.js
@@ -1,13 +1,12 @@
 import pkg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: "ce340redo_user",
-    host: "dpg-d82j5bgg4nts73b27o5g-a.oregon-postgres.render.com",
-    database: "ce340redo",
-    password: "yphBIumABUHCgqEBqg6dXRUuMCLpJ2C9",
-    port: 5432,
+    connectionString: process.env.DB_URL,
     ssl: {
         rejectUnauthorized: false
     }
